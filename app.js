05de2c6e12473d68c353000126c7e19894f639e7
angel-diego <angeldiegoagustin@gmail.com>
2023-07-11 22:44:47 -0300

    agregue nuevas lineas al archivo app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
 
 app.get("/", (req, res) => {
-    res.sendFile("/home/diego/Escritorio/web-node/index.html")
+    //res.sendFile("/home/diego/Escritorio/web-node/index.html")
+    res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.listen(3000, () => {
